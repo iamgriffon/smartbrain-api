62af13ad394c620d40ca902a04e7ab5dfeb9177d
Gustavo Dupin <55361396+iamgriffon@users.noreply.github.com>
2020-06-16 14:30:23 -0300

    image recon api fix #1

--- Controllers/image.js
+++ Controllers/image.js
@@ -6,7 +6,7 @@ const app = new Clarifai.App({
 const handleApiCall = (req, res) => {
       app.models.predict( Clarifai.FACE_DETECT_MODEL, req.body.input)
     .then(data => {
-        res.json(data);
+        return res.json(data);
     })
     .catch(err => res.status(400).json('Error'))}
 
